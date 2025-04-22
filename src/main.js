4c56567057709266b54d750458fde177d60d6ee6
Isam Ruhomutally <isamruhomutally@Isams-MacBook-Air.local>
2025-04-22 17:38:16 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,77 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <img src="/beaverbuffdetails_logo.png" alt="My Logo" class="logo" />  
+  <h1>Welcome to Beaver Buff Details</h1>
+    <p> We aim to serve everyone's needs, budgets, and time constraints. Services provided mobile or in studio. Website is currently under construction, below has some listed services.</p>
+
+    <section>
+      <h2> Polishing Promo</h2>
+      <p><strong>Pricing:</strong> Sedans: $300 | SUVs: $350</p>
+      <ul>
+        <li>Full exterior wash</li>
+        <li>Clay bar</li>
+        <li>1 stage paint correction, removing 70%+ of defects</li>
+        <p>Will be topped with either a paint sealant or can be upgraded to a 3 year ceramic coating </p>
+      </ul>
+    </section>
+
+    <section>
+      <h2>Full Interior Detail</h2>
+      <p><strong>Time:</strong> 3–5+ hours depending on condition</p>
+      <p><strong>Pricing:</strong> Sedan/Coupe: $130 | SUV: $160 | Work Trucks / 3-Row SUVs: $175</p>
+      <p><em>*We prioritize both appearance and cleanliness</em></p>
+      <ul>
+        <li>Weather Matts</li>
+        <li>Complete dusting, compressed air blow out, and vacuum</li>
+        <li>Shampoo carpet and carpet matts</li>
+        <li>Complete cleaning of all interior surfaces</li>
+        <li>Streak-free interior & exterior glass</li>
+        <li>3-month protective dressing to leather, plastic, and rubbers (UV, antimicrobial, water/dust-repelling)</li>
+      </ul>
+    </section>
+
+    <section>
+      <h2>Full Exterior Detail</h2>
+      <p><strong>Time:</strong> 1.5–3 hours depending on condition</p>
+      <p><strong>Pricing:</strong> Sedans: $110 | SUVs: $120</p>
+      <p><em>*Safety is our priority. All washes are subject to thorough foam and pressure wash before any hand washing contact is made. Vehicles are air dried and pat dry with a dedicated plush drying towel.</em></p>
+      <ul>
+        <li>Wheels, tires, and wheel wells deep cleaned</li>
+        <li>Hand wash and debug</li>
+        <li>Detailed brush cleaning of tight areas (grille, badges, window park, trim)</li>
+        <li>Exterior plastic and tires dressed with CarPro Perl</li>
+        <li>High gloss Sealant (3-6 months protection) applied to paint, windows, and wheels</li>
+        <li>Door jambs cleaned</li>
+      </ul>
+    </section>
+
+    <section>
+      <h2> Maintenance Interior Detail</h2>
+      <p><strong>Pricing:</strong> Sedans: $50 | SUVs: $60</p>
+      <ul>
+        <li>Mats Cleaned</li>
+        <li>Dashboard, center console, and seats wiped</li>
+        <li>Footwells quickly vacuum</li>
+        <li>Interior glass cleaned</li>
+      </ul>
+    </section>
+
+    <section>
+      <h2> Maintenance Exterior Detail</h2>
+      <p><strong>Pricing:</strong> Sedans: $50 | SUVs: $60</p>
+      <ul>
+        <li>Hand wash</li>
+        <li>3 month sealant, enhances gloss and paint protection</li>
+        <p>A convenient option for a quick and efficient hand wash of the exterior, excluding wheel cleaning and intricate areas</p>
+      </ul>
+    </section>
+
+    <section>
+      <h2>Contact</h2>
+      <p>Phone: <a href="tel:6472203001">(647) 220-3001</a></p>
+      <p>Instagram: <a href="https://instagram.com/beaverbuffdetails" target="_blank">@BeaverBuffDetails</a></p>
+    </section>
+  </div>
+`
